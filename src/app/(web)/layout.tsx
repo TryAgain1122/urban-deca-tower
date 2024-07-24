01175168ab425923cb578f676ca8494947fc3bbf
TryAgain1122 <luisrafhael1122@gmail.com>
2024-07-24 18:15:57 +0800

    undo

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Urban deca tower',
+  title: 'Hotel Management App',
   description: 'Discover the best hotel rooms',
 };
 

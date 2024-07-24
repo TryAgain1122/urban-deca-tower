10eebaeb6b5f6562b0ec0e7bdca2302536d5a0d9
TryAgain1122 <luisrafhael1122@gmail.com>
2024-07-24 18:22:00 +0800

    new changes

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Hotel Management App',
+  title: 'Urban deca tower',
   description: 'Discover the best hotel rooms',
 };
 
